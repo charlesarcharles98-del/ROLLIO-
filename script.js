1cbb85d7aef2a3d3c5b2cbd9d9c2ba201c558f21
charlesarcharles98-del <charlesarcharles98@gmail.com>
2026-05-27 15:02:03 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card, i) => {
+    card.style.opacity = "0";
+    card.style.transform = "translateY(30px)";
+
+    setTimeout(() => {
+      card.style.transition = "0.6s ease";
+      card.style.opacity = "1";
+      card.style.transform = "translateY(0)";
+    }, i * 150);
+  });
+
+});
